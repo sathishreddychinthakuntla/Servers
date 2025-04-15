397740350dac86cc47ecbec7dbfdaeca031fa8b9
sathishreddychinthakuntla <sathishreddych96@gmail.com>
2025-04-15 15:43:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 5
 ami = "ami-07a6f770277670015"
 instance_type = "t2.micro"
 key_name = "myserver"
-vpc_security_group_ids = ["sg-0b6cdb3cdf0c0a68b"]
+vpc_security_group_ids = ["sg-0589cf1c342ef9ece"]
 tags = {
 Name = var.instance_names[count.index]
 }
